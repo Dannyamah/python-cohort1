e8375b334bfa4ad45a10fa00db463de8c972c7e9
Daniel Amah <dannyamah@gmail.com>
2025-07-24 17:35:36 +0100

    Updated Learning

---
+++ crypto_utils.py
@@ -0,0 +1,18 @@
+class Wallet:
+    # Constructor
+    def __init__(self, owner):
+        self.owner = owner
+        self.balance = {}
+
+    def deposit(self, token, amount):  # We add self to allow us access attributes in the constructor
+        self.balance[token] = self.balance.get(token, 0) + amount
+
+    def withdraw(self, token, amount):
+        if self.balance.get(token, 0) >= amount:
+            self.balance[token] -= amount
+            return True
+        else:
+            return False
+
+    def view_balance(self):
+        return self.balance
---
+++ main.py
@@ -0,0 +1,12 @@
+from crypto_utils import Wallet
+
+my_wallet = Wallet("Joseph")
+my_wallet.deposit("Eth", 0.7)
+my_wallet.deposit("BTC", 1)
+print(my_wallet.view_balance())
+
+# wallet = CryptoWallet("Joseph")
+# wallet.deposit("ETH", 0.7)
+# wallet.deposit("BTC", 0.1)
+
+# print(wallet.view_balance())
